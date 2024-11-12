271e876b8b079600014b2da43b2e3511098040f9
Aleksandr Kim <112456407+KimAleksandr09041988@users.noreply.github.com>
2024-11-12 14:54:37 +0800

    module_9_2 hotfix
    module_9_2 hotfix

--- module_9_2.py
+++ module_9_2.py
@@ -1,7 +1,7 @@
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
 
-first_result = [len(item) for item in first_strings if len(item) < 5]
+first_result = [len(item) for item in first_strings if len(item) >= 5]
 print(first_result)
 
 second_result = [(item_1, item_2) for item_1 in first_strings for item_2 in second_strings if len(item_1) == len(item_2)]
